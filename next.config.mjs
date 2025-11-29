6bc4954f5bb63b02689873b219068538381f8cf4
dell dell <dell25252025@gmail.com>
2025-11-29 17:00:22 +0000

    npx next build 2

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Your existing configuration can go here.
+  // By not including "output: 'export'", we default to server-side rendering.
+};
+
+export default nextConfig;
