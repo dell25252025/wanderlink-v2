3cd48115c19b39736dfb495242f6e3c708b4f75e
dell dell <dell25252025@gmail.com>
2025-11-30 17:00:29 +0000

    BridgeActivity

--- android/app/src/main/java/com/wanderlink/app/MainActivity.java
+++ android/app/src/main/java/com/wanderlink/app/MainActivity.java
@@ -1,37 +1,5 @@
 package com.wanderlink.app;
 
-import android.os.Bundle;
-import android.webkit.PermissionRequest;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    WebView webView = (WebView) getBridge().getWebView();
-
-    if (webView != null) {
-      WebSettings settings = webView.getSettings();
-      settings.setJavaScriptEnabled(true);
-      settings.setDomStorageEnabled(true);
-      settings.setDatabaseEnabled(true);
-      settings.setMediaPlaybackRequiresUserGesture(false);
-      settings.setAllowFileAccess(true);
-      settings.setAllowContentAccess(true);
-      settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-      WebView.setWebContentsDebuggingEnabled(true);
-
-      webView.setWebChromeClient(new WebChromeClient() {
-        @Override
-        public void onPermissionRequest(final PermissionRequest request) {
-          // Grant microphone/camera permission to the webview origin
-          runOnUiThread(() -> request.grant(request.getResources()));
-        }
-      });
-    }
-  }
-}
+public class MainActivity extends BridgeActivity {}
