064e0edbc61f9437040db5a54ca91a9fe2a740ed
dell dell <dell25252025@gmail.com>
2025-11-30 11:10:21 +0000

    Lancer l'application

--- src/capacitor.config.ts
+++ src/capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
-  webDir: 'out',
-  server: {
-    url: 'https://wanderlink-v2--wanderlink-c1a35.us-east4.hosted.app',
-    cleartext: true
-  }
+  webDir: 'out'
 };
 
 export default config;
