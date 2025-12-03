95fb3972a99648a8b34f9fe6e6fa9b480e4e646b
Your Name <you@example.com>
2025-12-03 16:22:57 +0000

    avec url

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,16 +1,9 @@
-
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
-  webDir: 'src',
-  server: {
-    hostname: 'localhost',
-    url: 'http://localhost:3000',
-    cleartext: true,
-    androidScheme: 'https'
-  }
+  webDir: 'out'
 };
 
 export default config;
