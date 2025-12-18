3b3f32d386d4f30871a988823e2d65c80ce54b2e
Your Name <you@example.com>
2025-12-18 18:03:48 +0000

    j'ai testé et on peut toujours faire les capture d'ecran sur la page de

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/android/app/src/main/java/com/wanderlink/app/MainActivity.java
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/android/app/src/main/java/com/wanderlink/app/MainActivity.java
@@ -13,15 +13,13 @@ import androidx.annotation.NonNull;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // Log pour vérifier que cette méthode est bien exécutée au lancement.
-        android.util.Log.e("FLAG_SECURE_TEST", "onCreate MainActivity executing");
-        
-        // Appliquer le drapeau de sécurité AVANT l'initialisation de l'activité parente
-        // C'est la méthode la plus robuste pour s'assurer que le flag n'est pas écrasé.
+        // Appliquer le drapeau de sécurité AVANT l'initialisation de l'activité parente.
+        // C'est la méthode la plus robuste pour s'assurer que le flag est appliqué.
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_SECURE,
             WindowManager.LayoutParams.FLAG_SECURE
         );
+        android.util.Log.e("FLAG_SECURE_TEST", "onCreate MainActivity executing");
 
         super.onCreate(savedInstanceState);
     }
