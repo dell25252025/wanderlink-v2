b2483fe2006d7691b90a4b85ce50bd93ae6b160e
Your Name <you@example.com>
2025-12-31 16:05:47 +0000

    compte google 13

--- src/components/capacitor-setup.tsx
+++ src/components/capacitor-setup.tsx
@@ -5,15 +5,11 @@ import { useEffect } from 'react';
 import { Capacitor } from '@capacitor/core';
 import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 
-// Ce composant ne rend rien. Son seul but est d'initialiser les plugins
-// au démarrage de l'application sur les plateformes natives.
-
 const CapacitorSetup = () => {
   useEffect(() => {
     if (Capacitor.isNativePlatform()) {
       console.log('Initialisation de Capacitor...');
-      // Initialise le plugin Google Auth au démarrage
-      // [CORRECTION] Utilisation du bon Web Client ID de votre projet Firebase
+      // [CORRECTION] Utilisation du Web Client ID de votre projet Firebase
       GoogleAuth.initialize({
         clientId: '186522309970-kimg8pa9cd9lrmbl9uajk129nb0lrre2.apps.googleusercontent.com',
         scopes: ['profile', 'email'],
