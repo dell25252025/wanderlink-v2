9fa7abc1d568e9aefd826dab1033f5576cf25fa8
Your Name <you@example.com>
2026-03-03 20:14:19 +0000

    notification 4

--- android/app/src/main/java/com/wanderlink/app/MainActivity.java
+++ android/app/src/main/java/com/wanderlink/app/MainActivity.java
@@ -1,56 +1,17 @@
-
 package com.wanderlink.app;
 
 import android.os.Bundle;
-import android.webkit.PermissionRequest;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-
-import androidx.annotation.NonNull;
-
 import com.getcapacitor.BridgeActivity;
+// Importez le plugin PushNotifications
+import com.capacitorjs.plugins.pushnotifications.PushNotificationsPlugin;
 
 public class MainActivity extends BridgeActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // LE CODE QUI CRÉAIT LE CRASH A ÉTÉ SUPPRIMÉ D'ICI
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        WebView webView = this.bridge.getWebView();
-
-        if (webView != null) {
-            WebSettings settings = webView.getSettings();
-            
-            settings.setJavaScriptEnabled(true);
-            settings.setDomStorageEnabled(true);
-            settings.setDatabaseEnabled(true);
-            settings.setMediaPlaybackRequiresUserGesture(false);
-            settings.setAllowFileAccess(true);
-            settings.setAllowContentAccess(true);
-            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-
-            WebView.setWebContentsDebuggingEnabled(true);
-
-            webView.setWebChromeClient(new WebChromeClient() {
-                @Override
-                public void onPermissionRequest(final PermissionRequest request) {
-                    runOnUiThread(() -> {
-                        request.grant(request.getResources());
-                    });
-                }
-            });
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Enregistrez le plugin ici
+        registerPlugin(PushNotificationsPlugin.class);
     }
 }
