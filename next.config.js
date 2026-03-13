f70745fa8889ae3b2846e322c3d561fc3a0d51a2
Your Name <you@example.com>
2026-03-13 21:34:27 +0000

    test notification a et b 16

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Indiquer à Next.js où se trouve le tsconfig pour le build
   typescript: {
     // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
+    // Autoriser les builds de production à réussir même en cas d'erreurs de type.
     // !! WARN !!
     ignoreBuildErrors: true,
   },
 
   webpack: (config, { isServer }) => {
-    // Configuration pour les alias de chemin
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    // Configuration pour les alias de chemin - Méthode SÛRE
+    // On ajoute simplement le dossier 'src' aux endroits où Webpack cherche les modules.
+    config.resolve.modules.push('src');
 
     // Configuration pour ignorer le dossier 'functions' lors du watch
     if (!config.watchOptions) {
