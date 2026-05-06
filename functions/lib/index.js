44d1bf706309268b2a382e23f2ce526d53d38417
Your Name <you@example.com>
2026-05-06 16:11:23 +0000

    test like 1

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateAgoraToken = exports.sendNewMessageNotification = void 0;
+exports.onNotificationCreated = exports.sendNewMessageNotification = exports.generateAgoraToken = void 0;
 const admin = require("firebase-admin");
 admin.initializeApp();
-var notifications_1 = require("./notifications");
-Object.defineProperty(exports, "sendNewMessageNotification", { enumerable: true, get: function () { return notifications_1.sendNewMessageNotification; } });
-var agora_1 = require("./agora");
+// Importe et ré-exporte les fonctions individuellement pour un meilleur suivi
+// Fonction pour le token Agora (existante)
+const agora_1 = require("./agora");
 Object.defineProperty(exports, "generateAgoraToken", { enumerable: true, get: function () { return agora_1.generateAgoraToken; } });
+// Fonctions pour les notifications
+const notifications_1 = require("./notifications");
+Object.defineProperty(exports, "sendNewMessageNotification", { enumerable: true, get: function () { return notifications_1.sendNewMessageNotification; } });
+Object.defineProperty(exports, "onNotificationCreated", { enumerable: true, get: function () { return notifications_1.onNotificationCreated; } });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,14 @@
+
 import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
-export { sendNewMessageNotification } from "./notifications";
-export { generateAgoraToken } from "./agora";
+// Importe et ré-exporte les fonctions individuellement pour un meilleur suivi
+
+// Fonction pour le token Agora (existante)
+import { generateAgoraToken } from "./agora";
+export { generateAgoraToken };
+
+// Fonctions pour les notifications
+import { sendNewMessageNotification, onNotificationCreated } from "./notifications";
+export { sendNewMessageNotification, onNotificationCreated };
