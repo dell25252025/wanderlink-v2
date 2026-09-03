4d4beedd81b69a8f318ee7d1ebdaf002ff5a9322
Your Name <you@example.com>
2026-09-03 16:34:57 +0000

    test admob 6

--- android/app/src/main/java/com/wanderlink/app/MainActivity.java
+++ android/app/src/main/java/com/wanderlink/app/MainActivity.java
@@ -4,18 +4,44 @@ import android.os.Bundle;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.widget.RelativeLayout;
 
 import com.getcapacitor.BridgeActivity;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
 import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
+
+    private AdView adView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        MobileAds.registerWebView(this.bridge.getWebView());
+        // Initialisation du SDK Google Mobile Ads
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
 
+        // Nous allons maintenant charger la bannière de manière programmatique
+        RelativeLayout container = findViewById(R.id.container);
+        adView = new AdView(this);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/9214589741");
+        container.addView(adView);
+
+        loadBanner();
+
+        // Logique existante pour les canaux de notification
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel(
                 "messages",
@@ -30,9 +56,57 @@ public class MainActivity extends BridgeActivity {
         }
     }
 
+    private void loadBanner() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        AdSize adSize = getAdSize();
+        adView.setAdSize(adSize);
+
+        adView.loadAd(adRequest);
+    }
+
+    private AdSize getAdSize() {
+        // Determine the screen width (less decorations) to use for the ad width.
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+
+        float widthPixels = outMetrics.widthPixels;
+        float density = outMetrics.density;
+
+        int adWidth = (int) (widthPixels / density);
+
+        // Get a portrait-anchored adaptive banner ad size.
+        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
+    }
+
     @Override
-    public void onStart() {
+    protected void onStart() {
         super.onStart();
         registerPlugin(CallKitPlugin.class);
+    }
+
+    @Override
+    protected void onPause() {
+        if (adView != null) {
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (adView != null) {
+            adView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
     }
 }
